3e6c4ca20dd31b657a3017382776edbebcbe8230
Yassine Ben Slimane <yassine.benslimane@nostatikmedia.com>
2022-04-21 15:26:04 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const App: React.FC = () => {
+  const [isInDetail, setIsInDetail] = useState(false);
+
   return (
     <div className="flex flex-row flex-wrap min-h-screen inkdrop-app">
-      <nav className="flex flex-col w-[15%] bg-gray-500 border border-right">
-      Notebooks
-      </nav>
+      <button onClick={() => setIsInDetail((isInDetail) => !isInDetail)}>
+        Trigger
+      </button>
+      <div className="flex flex-col w-[15%] bg-gray-500 border border-right">
+        <AnimatePresence initial={false} exitBeforeEnter>
+          {isInDetail && (
+            <motion.div>
+              <span>Notebooks</span>
+              <button>Detail</button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
       <aside className="flex flex-col w-[20%] bg-gray-600 border border-right">
         Notes
       </aside>
